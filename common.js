9d87ab5388aea874f690c1fe54b3a96a3857a1fb
kimsunyoung03 <ksy.dmonster@gmail.com>
2024-07-23 09:19:45 +0900

    Add files via upload

--- common.js
+++ common.js
@@ -12,3 +12,12 @@ const setVh = () => {
       event.preventDefault();
     }
 }, false);
+
+var lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', function (event) {
+    var now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
