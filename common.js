76730423dfcc550d76be7e6be4e6bdf8edf6131a
kimsunyoung03 <ksy.dmonster@gmail.com>
2024-07-24 16:46:47 +0900

    Add files via upload

--- common.js
+++ common.js
@@ -1,10 +1,28 @@
-/* vh 브라우저 UI무시 */
-const setVh = () => {
-    document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`)
-  };
-  window.addEventListener('resize', setVh);
-  setVh();
+window.addEventListener('resize', () => setScreenSize());
 
+window.onload = function() {
+  
+  /* vh 브라우저 UI무시 */
+let vh = 0;
+const setVh = () => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  };
+window.addEventListener('resize', setVh);
+setVh();
+
+};
+document.addEventListener('DOMContentLoaded', function() {
+  
+/* vh 브라우저 UI무시 */
+let vh = 0;
+const setVh = () => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  };
+window.addEventListener('resize', setVh);
+setVh();
+})
 
 //   /* iOS 10 핀치줌 막기 */
 //   document.documentElement.addEventListener('touchstart', function (event) {
